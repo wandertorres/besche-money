2dacd1100572720f87765125a5b71214eb16a548
Wander Torres <wanderats@gmail.com>
2023-01-17 14:54:54 -0300

    feat: configure project styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-export default function App() {
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
+export function App() {
   return (
-    <h1>Besche Money</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      
+      <h1>Besche Money</h1>
+    </ThemeProvider>
   )
 }
