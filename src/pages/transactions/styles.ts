96a33555a108e46df88dd929ade5a88f0c478fe0
Wander Torres <wanderats@gmail.com>
2023-01-18 19:16:25 -0300

    feat: add searchform component

--- src/pages/transactions/styles.ts
+++ src/pages/transactions/styles.ts
@@ -11,11 +11,50 @@ export const TransactionsContainer = styled.main`
   padding: 0 1.5rem;
 `
 
+export const SearchForm = styled.form`
+  display: flex;
+  gap: 1rem;
+
+  input {
+    flex: 1;
+    border-radius: 6px;
+    border: 0;
+    background: ${props => props.theme["gray-900"]};
+    padding: 1rem;
+
+    &::placeholder {
+      color: ${props => props.theme["gray-500"]};
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+
+    border: 0;
+    padding: 1rem;
+    background: transparent;
+    border: 1px solid ${props => props.theme["green-300"]};
+    color: ${props => props.theme["green-300"]};
+    font-weight: bold;
+    border-radius: 6px;
+    cursor: pointer;
+
+    &:hover {
+      background: ${props => props.theme["green-500"]};
+      border-color: ${props => props.theme["green-500"]};
+      color: ${props => props.theme["white"]};
+      transition: background-color 0.2s, color 0.2s, border-color 0.2s;
+    }
+  }
+`
+
 export const TransactionsTable = styled.table`
   width: 100%;
   border-collapse: separate;
   border-spacing: 0 0.5rem;
-  /* margin-top: 1.5rem; */
+  margin-top: 1.5rem;
 
   td {
     padding: 1.25rem 2rem;
